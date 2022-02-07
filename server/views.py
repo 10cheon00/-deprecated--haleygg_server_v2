a9d71e35f999901a5a9c0b350035733f87d8dc6e
10cheon00 <10cheon00@gmail.com>
2022-02-08 03:52:16 +0900

    Refactor: Use custom filterset class to filter matches with player's profiles. To filter related model, using ModelMultipleChoiceFilter is answer. Removed filter_fields and defined custom filterset class.

--- server/views.py
+++ server/views.py
@@ -1,13 +1,13 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.exceptions import ParseError
 from rest_framework.viewsets import ModelViewSet
 
+from server.mixins import MatchFilterMixin
 from server.models import League
 from server.models import Map
 from server.models import Match
 from server.models import Profile
 from server.serializers import MatchSerializer
 from server.serializers import MapSerializer
+from server.serializers import MatchSerializer
 from server.serializers import LeagueSerializer
 from server.serializers import ProfileSerializer
 
@@ -27,34 +27,19 @@ class MapViewSet(ModelViewSet):
     queryset = Map.objects.all()
 
 
-class MatchViewSet(ModelViewSet):
+class MatchViewSet(MatchFilterMixin, ModelViewSet):
     serializer_class = MatchSerializer
-    filter_backends = [
-        DjangoFilterBackend,
-    ]
-    filter_fields = ("league", "map", "is_melee_match")
     queryset = (
         Match.objects.select_related("league", "map")
         .prefetch_related("players", "players__profile")
         .all()
     )
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        profiles = self.query_params.getlist("profiles[]", None)
-        if profiles:
-            try:
-                filtered_queryset = queryset
-                for profile in profiles:
-                    filtered_queryset = filtered_queryset.filter(
-                        players__profile=profile
-                    )
-                queryset = filtered_queryset
-            except ValueError:
-                pass
 
-        return queryset
+    serializer_class = MatchSerializer
+    queryset = (
+        Match.objects.select_related("league", "map")
+        .prefetch_related("players", "players__profile")
+        .all()
+    )
 
-    def list(self, request, *args, **kwargs):
-        self.query_params = request.query_params
-        return super().list(request=request, args=args, kwargs=kwargs)
