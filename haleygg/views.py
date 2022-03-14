649573c6f2d0a139238b7fc317eb550343b35d7e
10cheon00 <10cheon00@gmail.com>
2022-03-14 20:29:34 +0900

    Fix: List method in MatchViewSet should return matches in descending order.

--- haleygg/views.py
+++ haleygg/views.py
@@ -77,6 +77,17 @@ class MatchViewSet(MatchFilterMixin, ModelViewSet):
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset().reverse())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class MatchSummaryView(MatchFilterMixin, GenericAPIView):
     queryset = (
