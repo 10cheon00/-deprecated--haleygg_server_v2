e8c8914579aba3d0949da80a0d4548761ca50493
10cheon00 <10cheon00@gmail.com>
2022-04-15 15:13:29 +0900

    test: Create player test

--- haleygg/tests/test_player.py
+++ haleygg/tests/test_player.py
@@ -14,8 +14,17 @@ class PlayerTestCase(APITestCase, HaleyggUrlPatternsTestMixin):
         response = self.client.post(self.url, self.player)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_create_a_player_using_invalid_joined_date(self):
+        self.player["joined_date"] = "9999-99-99"
+        response = self.client.post(self.url, self.player)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.player["joined_date"] = "19-01-01"
+        response = self.client.post(self.url, self.player)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_create_a_player_using_invalid_race(self):
-        self.player["favorate_race"] = "R"
+        self.player["favorate_race"] = "invalid_type"
         response = self.client.post(self.url, self.player)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -26,7 +35,16 @@ class PlayerTestCase(APITestCase, HaleyggUrlPatternsTestMixin):
         response = self.client.post(self.url, self.player)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_get_player_detail_using_name_lookup_field(self):
+    def test_retrieve_players(self):
+        self.client.post(self.url, self.player)
+        self.player["name"] = "Another Player"
+        self.client.post(self.url, self.player)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 1)
+
+    def test_retrieve_a_player_detail(self):
         response = self.client.post(self.url, self.player)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -36,7 +54,7 @@ class PlayerTestCase(APITestCase, HaleyggUrlPatternsTestMixin):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], self.player["name"])
 
-    def test_get_player_detail_using_case_insensitive_name_lookup_field(self):
+    def test_retrieve_player_detail_using_uppercase_name(self):
         response = self.client.post(self.url, self.player)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -46,3 +64,22 @@ class PlayerTestCase(APITestCase, HaleyggUrlPatternsTestMixin):
             )
         )
         self.assertEqual(response.data["name"], self.player["name"])
+
+    def test_update_a_player(self):
+        self.client.post(self.url, self.player)
+
+        player = {"name": "Updated Player", "favorate_race": "Z"}
+        response = self.client.put(
+            reverse("player-detail", kwargs={"name__iexact": self.player["name"]}),
+            player,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_destroy_a_player(self):
+        self.client.post(self.url, self.player)
+
+        response = self.client.delete(
+            reverse("player-detail", kwargs={"name__iexact": self.player["name"]}),
+            self.player,
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
